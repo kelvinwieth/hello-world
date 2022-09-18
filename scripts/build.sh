0e82fde9efe1a2f2ef067092f62fe14f9e2f0c6b
kelvinwieth <kelvin.wieth@partner.bmw.com.br>
2022-09-17 21:51:45 -0300

    fix scripts running path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 echo building...
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
-python3 ../src/hello.py
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+python3 ./src/hello.py
