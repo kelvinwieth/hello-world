3d5bede2c4f22939576d75d2841659dee7720b63
kelvinwieth <kelvin.wieth@partner.bmw.com.br>
2022-09-17 22:02:26 -0300

    use absolute path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
 echo building...
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
-
-python3 src/hello.py
+python3 ~/hello-world/src/hello.py
